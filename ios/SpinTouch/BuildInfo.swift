21b7bc9e153d7b3422a31babc3cad46ad4311c7d
Steve Shreeve <steve.shreeve@gmail.com>
2026-06-24 22:25:16 -0600

    Stamp BuildInfo for device build (56e2bdf)

--- ios/SpinTouch/BuildInfo.swift
+++ ios/SpinTouch/BuildInfo.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 enum BuildInfo {
-    static let builtAt = "2026-06-25 03:45:12 UTC"
-    static let gitCommit = "2e76965"
+    static let builtAt = "2026-06-25 04:25:09 UTC"
+    static let gitCommit = "56e2bdf"
     static var version: String {
         Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
     }
